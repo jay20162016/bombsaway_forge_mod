1726d4642fc2a1140eb8d14359eb577b783ee7da
Jay jayjay <jayjay.jay20162016@gmail.com>
2019-09-01 11:10:09 -0400

    fix everything that was wrecked by prev. commit

--- main/java/com/gmail/jayjayjay20162016/bombsaway/RadiationEffect.java
+++ main/java/com/gmail/jayjayjay20162016/bombsaway/RadiationEffect.java
@@ -1,10 +1,8 @@
 package com.gmail.jayjayjay20162016.bombsaway;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
-<<<<<<< HEAD
-=======
-import java.util.Map.Entry;
->>>>>>> d82681819d0b2992a8f0aeee2f249b88cd5d6626
 
 import javax.annotation.Nullable;
 
@@ -13,12 +11,8 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AbstractAttributeMap;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
-<<<<<<< HEAD
-=======
-import net.minecraft.entity.ai.attributes.IAttribute;
-import net.minecraft.entity.ai.attributes.IAttributeInstance;
->>>>>>> d82681819d0b2992a8f0aeee2f249b88cd5d6626
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.EffectType;
@@ -29,7 +23,6 @@ public class RadiationEffect extends Effect {
 	protected static final UUID MAX_HEALTH_UUID = UUID.fromString("66aa4f51-77db-485e-9380-967977c7d9f4");
 	protected static final DamageSource RADIOACTIVITY = (new DamageSource("radioactivity")).setDamageBypassesArmor()
 			.setDamageIsAbsolute();
-<<<<<<< HEAD
 
 	public RadiationEffect() {
 		super(EffectType.HARMFUL, 65280);
@@ -37,25 +30,13 @@ public class RadiationEffect extends Effect {
 				AttributeModifier.Operation.ADDITION);
 	}
 
-	/*
-	 * /// CRASHES ON MILK 
-	   public List<ItemStack> getCurativeItems() { return new ArrayList<ItemStack>(); }
-	 */
-	public void performEffect(LivingEntity entityLivingBaseIn, int amplifier) {
-		entityLivingBaseIn.attackEntityFrom(RADIOACTIVITY, (amplifier + 1) / 2);
-=======
-	private int damage;
-
-	public RadiationEffect(int damage) {
-		super(EffectType.HARMFUL, 65280);
-		this.damage = damage;
+	/// CRASHES ON MILK
+	public List<ItemStack> getCurativeItems() {
+		return new ArrayList<ItemStack>();
 	}
 
 	public void performEffect(LivingEntity entityLivingBaseIn, int amplifier) {
-		entityLivingBaseIn.attackEntityFrom(RADIOACTIVITY, this.damage / 2);
-		entityLivingBaseIn.getAttribute(SharedMonsterAttributes.MAX_HEALTH).applyModifier(new AttributeModifier(
-				"5D6F0BA2-1186-46AC-B896-C61C5CEE99CC", -8.0D, AttributeModifier.Operation.ADDITION));
->>>>>>> d82681819d0b2992a8f0aeee2f249b88cd5d6626
+		entityLivingBaseIn.attackEntityFrom(RADIOACTIVITY, amplifier / 2);
 		entityLivingBaseIn.addPotionEffect(new EffectInstance(Effects.HUNGER));
 	}
 
@@ -64,11 +45,7 @@ public class RadiationEffect extends Effect {
 		super.removeAttributesModifiersFromEntity(entityLivingBaseIn, attributeMapIn, amplifier);
 		if (entityLivingBaseIn instanceof PlayerEntity) {
 			PlayerEntity player = (PlayerEntity) entityLivingBaseIn;
-<<<<<<< HEAD
 			player.addPotionEffect(new EffectInstance(EffectList.radiationsick, (amplifier + 1) * 250000));
-=======
-			player.addPotionEffect(new EffectInstance(EffectList.radiationsick, 4000)); // instead of null put in effectinstance
->>>>>>> d82681819d0b2992a8f0aeee2f249b88cd5d6626
 
 		}
 	}
--- main/java/com/gmail/jayjayjay20162016/bombsaway/RadiationSicknessEffect.java
+++ main/java/com/gmail/jayjayjay20162016/bombsaway/RadiationSicknessEffect.java
@@ -1,24 +1,23 @@
 package com.gmail.jayjayjay20162016.bombsaway;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AbstractAttributeMap;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Effect;
-<<<<<<< HEAD
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.EffectType;
 import net.minecraft.potion.Effects;
-=======
-import net.minecraft.potion.EffectType;
->>>>>>> d82681819d0b2992a8f0aeee2f249b88cd5d6626
 
 public class RadiationSicknessEffect extends Effect {
 
 	public RadiationSicknessEffect() {
 		super(EffectType.HARMFUL, 65280);
-<<<<<<< HEAD
 		this.addAttributesModifier(SharedMonsterAttributes.MAX_HEALTH, "5D6F0BA2-1186-46AC-B896-C61C5CEE99CC", -8.0D,
 				AttributeModifier.Operation.ADDITION);
 
@@ -28,24 +27,16 @@ public class RadiationSicknessEffect extends Effect {
 		entityLivingBaseIn.addPotionEffect(new EffectInstance(Effects.HUNGER));
 	}
 
-	/*
-	 * /// DOESN'T CRASH ON MILK AND IS REMOVED BY MILK public List<ItemStack>
-	 * getCurativeItems() { return new ArrayList<ItemStack>(); }
-	 */
-=======
+	/// DOESN'T CRASH ON MILK AND IS REMOVED BY MILK
+	public List<ItemStack> getCurativeItems() {
+		return new ArrayList<ItemStack>();
 	}
 
->>>>>>> d82681819d0b2992a8f0aeee2f249b88cd5d6626
 	public void removeAttributesModifiersFromEntity(LivingEntity entityLivingBaseIn,
 			AbstractAttributeMap attributeMapIn, int amplifier) {
 		super.removeAttributesModifiersFromEntity(entityLivingBaseIn, attributeMapIn, amplifier);
 		if (entityLivingBaseIn instanceof PlayerEntity) {
 			PlayerEntity player = (PlayerEntity) entityLivingBaseIn;
-<<<<<<< HEAD
-=======
-			player.getAttribute(SharedMonsterAttributes.MAX_HEALTH).removeModifier(new AttributeModifier(
-					"5D6F0BA2-1186-46AC-B896-C61C5CEE99CC", -8.0D, AttributeModifier.Operation.ADDITION));
->>>>>>> d82681819d0b2992a8f0aeee2f249b88cd5d6626
 		}
 	}
 }
